b0f28432c2326572025b5b0dd811f87fec6a1a9e
fering <fering@ubuntu.myguest.virtualbox.org>
2023-01-05 02:49:14 +0200

    Deploy website - based on 3fa75bafba268f8ad38e5623bcdc83685f74c02c

---
+++ assets/js/55960ee5.e765800b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfishing_wiki=self.webpackChunkfishing_wiki||[]).push([[121],{8070:i=>{i.exports=JSON.parse('[{"label":"\u0432 \u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0435","permalink":"/fishing-wiki/docs/tags/\u0432-\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0435","count":3}]')}}]);
